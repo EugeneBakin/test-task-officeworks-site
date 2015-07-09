07cff5a3e8c2925ca2fad2ae8c1a032bb37fd641
EugeneBakin <bakin.eugene@ya.ru>
2015-07-09 17:50:55 +0500

    [JS] HashRouter + tests

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-;
+var Main = {
+	HashRouter: function HashRouter ( startState, location, window ) {
+
+		var pubsub = $({});
+		var currentState = '';
+		var publish = function publish (topic, data) {
+			pubsub.trigger(topic, data);
+		}; 
+		var subscribe = function subscribe (topic, cb) {
+			pubsub.on(topic, cb);
+		};
+		var setState = function setState (state) {
+			if (currentState !== state) {
+				currentState = state;
+				location.hash = '#' + state;
+				publish('state/changed', state);
+			}
+		};
+
+		setState(startState);
+
+		$(window).on('hashchange', function() {
+			setState(location.hash.slice(1));
+		});
+
+		return {
+			subscribe: subscribe,
+			setState: setState
+		};
+	}
+};
--- js/tests.js
+++ js/tests.js
@@ -1,27 +1,42 @@
-var test = function () {};
 var tests = {
-	'router' : {
-		'check': function (assert) {
-			assert.strictEqual(1,1);
-		}
-	},
-	'not router' : {
-		'check 2' : function (assert) {
-			assert.strictEqual(1,'1');
-		} 
-	}
-};
-
-$(function () {
-	test = function () {
-		$('#qunit').show();
-		for ( moduleName in tests ) {
-			console.log(moduleName, tests[moduleName]);
-			QUnit.module( moduleName );
-			for( testName in tests[moduleName] ) {
-				console.log(testName, tests[moduleName][testName]);
-				QUnit.test( testName, tests[moduleName][testName] );
+	'Router' : {
+		'before': function () {
+			this.fakeLocation = {
+				'hash': 'fake'
+			};
+			this.fakeWindow = {'location': this.fakeLocation};
+		},
+		'tests':{
+			'checkIfLocationIsSetToStartStateOnInit': function (assert) {
+				var router = Main.HashRouter( 'changed', this.fakeLocation, this.fakeWindow );
+				assert.strictEqual('#changed', this.fakeLocation.hash, 'hash changed to "#changed"' );
+			},
+			'checkIfLocationIsChangedOnEvent': function (assert) {
+				var done = assert.async();
+				var router = Main.HashRouter( 'state', this.fakeLocation, this.fakeWindow );
+				var $window = $(this.fakeWindow);
+				assert.strictEqual('#state', this.fakeLocation.hash, 'hash was equal to "#state" at start');
+				router.subscribe('state/changed', function (event, state) { 
+					assert.strictEqual(state, 'changedState', 'router triggered event "state/changed" with "changedState" argument');
+					done();
+				});
+				this.fakeLocation.hash = '#changedState';
+				$window.trigger('hashchange');
 			}
 		}
+	},
+	'empty module' : {}
+};
+
+var testJS = function () {
+	$('#qunit').show();
+	for ( moduleName in tests ) {
+		QUnit.module( moduleName, {
+			beforeEach: tests[moduleName]['before'] || $.noop,
+			afterEach: tests[moduleName]['after'] || $.noop
+		});
+		for( testName in tests[moduleName]['tests'] ) {
+			QUnit.test( testName, tests[moduleName]['tests'][testName] );
+		}
 	}
-});
+}
