e73109bd82cc979de9bd19276e1623772f257705
EugeneBakin <bakin.eugene@ya.ru>
2015-07-10 17:08:09 +0500

    Made simple views for all screens (showing/hiding)

--- js/client.js
+++ js/client.js
@@ -1,7 +1,10 @@
 var screen1;
+var screen2;
+var screen3;
+var screen4;
 
 $(function () {
-	var firstScreenView = {
+	var view1 = {
 		render: function () {
 			$('#choose-type').show();
 		},
@@ -10,5 +13,39 @@ $(function () {
 		}
 	};
 
-	screen1 = Main.State({},firstScreenView);
+	screen1 = Main.State({},view1);
+
+	var view2 = {
+		render: function() {
+			$('#pass-form').show();
+		},
+		destroy: function() {
+			$('#pass-form').hide()
+		}
+	};
+
+	screen2 = Main.State({},view2);
+
+	var view3 = {
+		render: function() {
+			$('#pass-confirm').show();
+		},
+		destroy: function() {
+			$('#pass-confirm').hide()
+		}
+	};
+
+	screen3 = Main.State({},view3);
+
+	var view4 = {
+		render: function() {
+			$('#pass-success').show();
+		},
+		destroy: function() {
+			$('#pass-success').hide()
+		}
+	};
+
+	screen4 = Main.State({},view4);
+
 })
